7ba931bded8f777772dad53ddd75c9b26d9b9a89
Linus Xu <linus_xu@live.com>
2024-04-16 00:21:47 -0400

    Adding capability to locate all focusable elements on page, for later steps.

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,15 @@
-const focusable = 'button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"]):not([disabled]), details:not([disabled]), summary:not(:disabled)';
+
+const test = () => {
+    console.log(document.querySelectorAll('button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"]):not([disabled]), details:not([disabled]), summary:not(:disabled)'));
+}
 
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.insertCSS({
         files: ["css/style.css"],
         target: { tabId: tab.id },
+    })
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: test
     })
 });
