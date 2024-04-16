12e967b9b6889a15ddaedd73c165f1ab8cff7f31
Linus Xu <linus_xu@live.com>
2024-04-16 00:15:53 -0400

    Simple focus functionality and preparations for later parts.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
-console.log('Test')
+const focusable = 'button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"]):not([disabled]), details:not([disabled]), summary:not(:disabled)';
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.insertCSS({
+        files: ["css/style.css"],
+        target: { tabId: tab.id },
+    })
+});
