1bfacb27d401c2760599a2064bc2c0908a801d6d
Linus Xu <linus_xu@live.com>
2024-04-18 00:27:32 -0400

    Some extra foundation for the later parts of the extension. Should work for the presentation...

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
-
 const test = () => {
-    console.log(document.querySelectorAll('button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"]):not([disabled]), details:not([disabled]), summary:not(:disabled)'));
+    const focusables = [];
+    document.querySelectorAll('button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"]):not([disabled]), details:not([disabled]), summary:not(:disabled)').forEach((focusable) => {
+        focusables.push(focusable);
+        focusable.addEventListener('focus', () => {
+            console.log(focusable.getBoundingClientRect());
+        })
+        focusable.addEventListener('blur', () => {
+            console.log(focusables.indexOf(focusable));
+        })
+    });
 }
 
 chrome.action.onClicked.addListener((tab) => {
